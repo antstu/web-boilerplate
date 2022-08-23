d70e36dcd06685d1740afe2d719aac0606750189
Anthony Stuvecke <anthony.stuvecke@gmail.com>
2022-08-23 17:04:29 +0200

    integrated cookies

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,12 @@ import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
 
+import * as redis from "redis";
+
+import session from "express-session";
+import { __prod__ } from "./constants";
+import { MyContext } from "./types";
+
 const main = async () => {
   // connects to db
   const orm = await MikroORM.init(mikroOrmConfig);
@@ -19,12 +25,35 @@ const main = async () => {
 
   const app = express();
 
+  let RedisStore = require("connect-redis")(session);
+  let redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+        // disableTTL: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        secure: __prod__, //cookie only works in https
+        sameSite: "lax", // csrf
+      },
+      saveUninitialized: false,
+      secret: "keyboard cat",
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em.fork() }),
+    context: ({ req, res }): MyContext => ({ em: orm.em.fork(), req, res }),
   });
 
   await apolloServer.start();
