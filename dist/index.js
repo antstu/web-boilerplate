239ed70a6150829e69f8682e7124a4502a6c800c
Anthony Stuvecke <anthony.stuvecke@gmail.com>
2022-08-23 13:27:26 +0200

    added apollo/graphql server

--- dist/index.js
+++ dist/index.js
@@ -4,13 +4,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
-const Posts_1 = require("./entities/Posts");
+const apollo_server_express_1 = require("apollo-server-express");
+const express_1 = __importDefault(require("express"));
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const type_graphql_1 = require("type-graphql");
+const hello_1 = require("./resolvers/hello");
 const main = async () => {
     const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
     await orm.getMigrator().up();
-    const post = orm.em.create(Posts_1.Post, { title: "my first post" });
-    await orm.em.persistAndFlush(post);
+    const app = (0, express_1.default)();
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: await (0, type_graphql_1.buildSchema)({
+            resolvers: [hello_1.HelloResolver],
+            validate: false,
+        }),
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+    app.listen(4000, () => {
+        console.log("server started on localhost:4000");
+    });
 };
 main().catch((error) => {
     console.log(error);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entities/Posts";
 
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
 import mikroOrmConfig from "./mikro-orm.config";
+
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   // connects to db
@@ -9,8 +13,22 @@ const main = async () => {
   //   runns migration
   await orm.getMigrator().up();
 
-  const post = orm.em.create(Post, { title: "my first post" });
-  await orm.em.persistAndFlush(post);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((error) => {
